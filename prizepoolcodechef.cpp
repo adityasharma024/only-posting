8de7a6ba482710cb18abeda9511c299078514731
ADITYA SHARMA <91861881+adityasharma024@users.noreply.github.com>
2022-09-11 19:07:11 +0530

    Add files via upload

---
+++ prizepoolcodechef.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,a,b;
+        cin>>x>>y;
+        a=x*10;
+        b=y*90;
+        int total=a+b;
+        cout<<total<<endl;
+    }
+}
